7f8cf3fdcdd606bcba1b787cff7db1fa25952bf4
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-04-03 14:35:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [status, setState] = React. useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        {
+          status ? <h1>Hello World !</h1> : null
+        }
+      <button onClick={()=> setState(false)}>Hide</button>
+      <button onClick={()=> setState(true)}>Show</button>
     </div>
-  );
+  )
 }
+
+
+// getting value from input
+// function App() {
+//   const [data, setData] = useState(null)
+//   const [print, setPrint] = useState(false)
+//   function getData(val) {
+//     console.log(val.target.value);
+//     setData(val.target.value)
+//   }
+//   return (
+//     <div className='App'>
+//       {
+//         print?  
+//         <h1>{data}</h1>   
+//         :null
+//       }
+//       <input type='text' onChange={getData} />
+//       <button onClick={() => setPrint(true)}> print Data </button>
+//     </div>
+//   );
+// }
 
 export default App;
